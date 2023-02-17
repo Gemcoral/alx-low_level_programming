5d26793b9034984be719b1bc82f5c499d05f7e56
Gemcoral <fsyntych@example.com>
2023-02-17 15:22:27 -0800

    Task 8: Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,14 +8,13 @@
 
 int main(void)
 {
-	char i;
+	int i;
+	char j;
 
-	for (i = 'a'; i <= 'z'; i++)
-		if (i != 'q' && i != 'e')
-			putchar(i);
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
 	putchar('\n');
 	return (0);
-}	
-
-	
-
+}
