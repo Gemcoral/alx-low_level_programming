2ef59d38fdcbe1e719bf8a032f0d409236313c21
Gemcoral <fsyntych@example.com>
2023-02-22 02:58:26 -0800

    Task 6: Absolut value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *@r: the integer to be printed
+ * Return: Always 0
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (-r);
+
+}
