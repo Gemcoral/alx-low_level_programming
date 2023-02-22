6e6dacafe5d34c8dae575ec6f0cf627dd19ded71
Gemcoral <fsyntych@example.com>
2023-02-22 14:48:35 -0800

    Task 4: isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,14 @@
 
 /**
  * _isalpha - function that checks for alphabetic character
- * @c: the char to be printed
+ * @c: the parameter to be printed
  * Return: If c is a letter, uppercase or lowercase return 1 otherwise 0
  */
 
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-			return (1);
+		return (1);
 	else
-			return (0);
-}	
+		return (0);
+}
