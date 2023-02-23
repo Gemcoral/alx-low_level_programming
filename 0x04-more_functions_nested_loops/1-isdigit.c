e00a89db4cd65c59893f73a0c4b476a0024b3a2a
Gemcoral <fsyntych@example.com>
2023-02-23 07:05:05 -0800

    Task 1: isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that checks for a digit (0 through 9)
+ * @c: char to be checked
+ * Return: if c is a digit return 1 otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c >= '9')
+		return (1);
+	else
+		return (0);
+}
