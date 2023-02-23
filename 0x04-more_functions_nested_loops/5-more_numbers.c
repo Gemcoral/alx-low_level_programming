416828595613711ae4484db03dda46eec6f3c96e
Gemcoral <fsyntych@example.com>
2023-02-23 14:38:25 -0800

    Task 4: I believe in numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints 10 times the number from 0-14
+ *
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1')
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');		
+	}
+}
