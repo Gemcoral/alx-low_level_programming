5469a7acc021adbd133cfb60940cb9ef93b48dc5
Gemcoral <fsyntych@example.com>
2023-02-23 11:20:26 -0800

    Task 3: Number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - function that prints numbers from 0-9
- *
+ *_putchar: used to print out the character
  * Return: void
  */
 
@@ -12,7 +12,7 @@ void print_numbers(void)
 
 	for (a = '0'; a <= '9'; a++)
 	{
-		_putchar(a);
+		_putchar(a + '0');
 	}
 	_putchar('\n');
 }
