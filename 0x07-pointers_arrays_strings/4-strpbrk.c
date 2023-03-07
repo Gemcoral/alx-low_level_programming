3902a1d350467b6181e52edc3a37f46e1f550847
Gemcoral <fsyntych@example.com>
2023-03-06 17:15:55 -0800

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,23 +1,24 @@
-#include "main.h"
 
+#include "main.h"
 /**
  * _strpbrk - Entry point
  * @s: input
  * @accept: input
  * Return: Always 0 (Success)
  */
-
 char *_strpbrk(char *s, char *accept)
 {
-	int j;
+		int k;
 
-	while (*s)
-	{
-		for (j = 0; accept[j]; j++)
+		while (*s)
 		{
-		if (*s == accept[j])
-		return (*s);
-	s++;	}
-	}
+			for (k = 0; accept[k]; k++)
+			{
+			if (*s == accept[k])
+			return (s);
+			}
+		s++;
+		}
+
 	return ('\0');
 }
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,17 @@
 #include "main.h"
-
 /**
  * print_chessboard - Entry point
  * @a: array
- * Return: Alwaya 0 (Success)
+ * Return: Always 0 (Success)
  */
-
 void print_chessboard(char (*a)[8])
 {
 	int i;
 	int j;
 
-	for (i = 0; i < 0; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j < 8, j++)
+		for (j = 0; j < 8; j++)
 			_putchar(a[i][j]);
 		_putchar('\n');
 	}
